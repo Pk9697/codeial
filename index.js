b29c411f722a7aa766b51c2e7ea68ec2b4a19ad9
Prashant Kumar <kumar.prashant152@gmail.com>
2021-05-31 16:45:59 +0530

    add home.ejs inside views folder and render it using home_controller

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use('/',require('./routes'));//middleware is used to load routes from path .
 app.set('view engine','ejs');//setting ejs view engine 
 //setting the folder where views folder will be located
 app.set('views','./views');//./views is the path where html files will be located instead of path.join
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);//backtics `` situated just left of key 1 -this is called interpolation where we embed our variables inside the string
