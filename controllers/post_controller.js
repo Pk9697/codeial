ea474f1673f99bf5e5dbc1c8c4dd639fda5e3f6f
Prashant Kumar <kumar.prashant152@gmail.com>
2021-06-14 16:59:31 +0530

    delete post only by authorized user

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post=require('../models/post');
+const Comment=require('../models/comment')
 module.exports.create=function(request,response){
     Post.create({
         content: request.body.content,
@@ -9,5 +10,21 @@ module.exports.create=function(request,response){
             return;
         }
         return response.redirect('back');
+    });
+}
+
+module.exports.destroy=function(request,response){
+    Post.findById(request.params.id,function(err,post){
+        //.id means converting the object id into string
+        if(post.user == request.user.id){//authorization if the delete request is from the same person who created it or not
+            post.remove();
+            //deleteMany is the fxn which deleted all the comments based on some query past
+            Comment.deleteMany({post: request.params.id}, function(err){//delete comments with post having that post id
+                return response.redirect('back');
+            });
+
+        }else{
+            return response.redirect('back');
+        }
     });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,6 @@ const postsController=require('../controllers/post_controller');
 const passport=require('passport');
 //only display post form when user is authenticated using passport
 router.post('/create',passport.checkAuthentication,postsController.create);//2nd level of check using passport 
+router.get('/destroy/:id', passport.checkAuthentication ,postsController.destroy);
 
 module.exports=router;
