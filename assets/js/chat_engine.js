15f856fb60ccf1a74c4d9afc87d8a8866e34f705
Prashant Kumar <kumar.prashant152@gmail.com>
2021-07-20 21:57:35 +0530

    setup the initial connection and create and join chat room using emit and on event

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -1,1 +1,31 @@
 //chat_engine here will be communicating from the client side on the browser whereas chat_sockets.js is the one which is going to be the observer or server which is going to receive the upcoming connections from all the users which are the subscribers
+class ChatEngine{
+    constructor(chatBoxId,userEmail){
+        this.chatBox=$(`#${chatBoxId}`);
+        this.userEmail=userEmail;
+        //this io.connect fires a connection event in chat_sockets in config
+        //*1
+        this.socket=io.connect('http://localhost:4000');//! i emit a connect event //1
+
+        if(this.userEmail){//if useremail exists for a user then only call connectionhandler
+            this.connectionHandler();
+        }
+    }
+    //this connection handler will have to and fro interaction bw observer and subscriber
+    //*3
+    connectionHandler(){ //! acknowledgement is emitted back from chat sockets that you are connected with connect event
+        let self=this;
+        this.socket.on('connect',function(){//on event on connect 
+            console.log('connection established using sockets');
+
+            self.socket.emit('join_room',{//join_room event with parameters as subscriber email and chatroom name as codeial
+                user_email: self.userEmail,
+                chatroom:'codeial'
+            });
+
+            self.socket.on("user_joined",function(data){
+                console.log("a user joined:", data);
+            })
+        })
+    }
+}
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,35 @@
 //chat_engine here will be communicating from the client side on the browser whereas chat_sockets.js is the one which is going to be the observer or server which is going to receive the upcoming connections from all the users which are the subscribers
 module.exports.chatSockets = function(socketServer){
+    // let io=require('socket.io')(socketServer);
+    
+// const io = require("socket.io")(socketServer, {
+//     cors: {
+//       origin: "http://localhost:5000",
+//       methods: ["GET", "POST"]
+//     }
+//   });
+    const io = require("socket.io")(socketServer, {
+        cors:{//important upgrade
+            origin: "*"
+        }
+    });
 
+    //connection event is fired from chat_engine io.connect
+    //*2
+    io.sockets.on('connection', function(socket){//! this receives a connection and this emits back the acknowledgement to chat_engine connectionhandler 
+        console.log('new connection received', socket.id);
+
+        socket.on("disconnect",function(){//* working with ""
+            console.log('socket disconnected!', socket.id);
+
+        });
+        //on event join_room execute call back fxn
+        socket.on("join_room",function(data){
+            console.log('joining request received :',data);
+            //when joining request has been received join that socket to room
+            socket.join(data.chatroom);//join the user in chat room
+            //when i join the chatroom everyone including me should receive notification that i joined the chatroom
+            io.in(data.chatroom).emit('user_joined',data);//emitting 'user_joined' event from server to every other subscriber
+        });
+    }); 
 }
